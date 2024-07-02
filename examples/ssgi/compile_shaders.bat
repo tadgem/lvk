65ba24ad644b76ccdfc8dafd576237bf5bfceb76
tadgem <98434757+tadgem@users.noreply.github.com>
2024-07-02 22:49:36 +0100

    stub accumulate shader

--- examples/ssgi/compile_shaders.bat
+++ examples/ssgi/compile_shaders.bat
@@ -5,3 +5,5 @@
 %VULKAN_SDK%/bin/glslc.exe shaders/lights.frag -o shaders/lights.frag.spv
 
 %VULKAN_SDK%/bin/glslc.exe shaders/SSGI.frag -o shaders/ssgi.frag.spv
+
+%VULKAN_SDK%/bin/glslc.exe shaders/accumulate.frag -o shaders/accumulate.frag.spv
