9d78e7041b602dc5762d589342f5c79f6f39903f
Tadge <98434757+tadgem@users.noreply.github.com>
2025-03-31 17:30:05 +0100

    allow turning off the LVK implementation

--- lvk/include/lvk/Log.h
+++ lvk/include/lvk/Log.h
@@ -1,7 +1,9 @@
 #pragma once
+#ifndef LVK_PRINTF_IMPL
 // TODO: More robust external logging support
 #include "stdio.h"
 #define LVK_PRINTF_IMPL(...)	printf(__VA_ARGS__);printf("\n")
 #define LVK_LOG_INFO(...)		printf("INFO: ");	LVK_PRINTF_IMPL(__VA_ARGS__)
 #define LVK_LOG_ERR(...)		printf("ERROR: ");	LVK_PRINTF_IMPL(__VA_ARGS__)
 #define LVK_LOG_WARN(...)		printf("WARN: ");	LVK_PRINTF_IMPL(__VA_ARGS__)
+#endif
